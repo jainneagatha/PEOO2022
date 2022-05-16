cec1b0fd33b5bf0ab0e27e05dd480509475ef0fa
JAINNE AGATHA <79118143+jainneagatha@users.noreply.github.com>
2022-05-16 18:19:40 +0000

    adicionei a pasta da lista 1

---
+++ AULA_1305/lista1.cs
@@ -0,0 +1,9 @@
+using System; 
+
+public class Program {
+
+  public static void Main(string[] args) {
+    Console.WriteLine("20201011110007");
+    Console.WriteLine("Jainne Agatha Costa Bezerra");
+  }
+}
